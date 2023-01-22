92396d21580459598aaddd6d57fd460b5eaf7d42
AleksandrDudik <saimonnightwalker@gmail.com>
2023-01-22 15:04:11 +0300

    First try

---
+++ scripts/scripts.js
@@ -0,0 +1,29 @@
+const popup = document.querySelector ('.popup');
+const buttonClose = document.querySelector ('.popup__button_actions_exit');
+const changeForm = document.querySelector ('.popup__container');
+const buttonEdit = document.querySelector ('.profile__button_actions_edit');
+const nameGet = document.querySelector ('.profile__name');
+const jobGet = document.querySelector ('.profile__profession');
+let nameInput = changeForm.querySelector ('.popup__input_name');
+let jobInput =  changeForm.querySelector ('.popup__input_job');
+
+function openPopup () {
+  popup.classList.add ('popup__opened');
+  nameInput.value = nameGet.textContent;
+  jobInput.value = jobGet.textContent;
+}
+
+function closePopup ()  {
+  popup.classList.remove ('popup__opened');
+}
+
+function formSubmit (evt) {
+    evt.preventDefault();
+    nameGet.textContent = `${nameInput.value}`;
+    jobGet.textContent = `${jobInput.value}`;
+    closePopup ();
+}
+
+changeForm.addEventListener('submit', formSubmit);
+buttonEdit.addEventListener ('click', openPopup);
+buttonClose.addEventListener ('click', closePopup);
